db24403633cf7813fe06db4b44d5f725842e4ff7
zanaelah <fauzan14072004@gmail.com>
2020-11-06 19:07:51 +0700

    project mvc

---
+++ app/views/about/index.php
@@ -0,0 +1,5 @@
+<div class="container">
+	<h1 class="mt-4">About Me</h1>
+	<img src="<?= BASEURL; ?>/img/profile.jpg" alt="Muhammad Fauzan" width="200" class="rounded-circle shadow">
+	<p>Halo, Nama saya <b><?= $data['nama']; ?></b>, saya seorang <b><?= $data['pekerjaan']; ?></b>, dan umur saya <b><?= $data['umur']; ?></b> tahun. </p>
+</div>
